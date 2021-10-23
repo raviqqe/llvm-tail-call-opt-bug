a569fc326baa91d5197b2f2bf431979d3ee9a9d8
Yota Toyama <raviqqe@gmail.com>
2021-10-23 22:53:44 +0000

    Refactor

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 
 set -ex
 
-llc -O3 -tailcallopt min-test-case.ll -filetype obj -o foo.o && llvm-objdump -d foo.o >foo.s
+llc -O3 -tailcallopt min-test-case.ll -filetype obj -o foo.o
+llvm-objdump -d foo.o >foo.s
