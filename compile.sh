0bc5548335c9ff568d0631f33867f11e935da2d9
Yota Toyama <raviqqe@gmail.com>
2022-02-04 16:51:09 +0000

    Treat warnings as errors

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-llc -O3 -tailcallopt min-test-case.ll -filetype obj -o foo.o
+llc -O3 --fatal-warnings -tailcallopt min-test-case.ll -filetype obj -o foo.o
 llvm-objdump -d foo.o >foo.s
 clang foo.o stub.c
